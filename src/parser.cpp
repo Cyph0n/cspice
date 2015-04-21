7b340f49a62617afda6c5de42b4e26e6ba8095ee
Assil Ksiksi <cyph0nik@gmail.com>
2015-04-21 19:23:51 +0400

    Refactored parse_line to use a template. Other minor changes to the parser.

--- src/parser.cpp
+++ src/parser.cpp
@@ -6,61 +6,95 @@ void Parser::parse() {
     // Skip first line of input; SPICE syntax spec
     getline(file, line);
 
+    line_no++;
+
     while (!file.eof()) {
         // Get a line of input
         getline(file, line);
 
-        // Iterate through regex patterns; if match found, determine
-        // which matched based on regex order in const vector
         Component* c;
+        Source* s;
 
-        unsigned char p = 0;
+        std::smatch matches;
 
-        for (auto reg: regexes) {
-            if (regex_search(line, reg)) {
-                switch (p) {
-                    case EMPTY_LINE:
-                        std::cout << "empty\n";
-                        break;
-                    case VDC:
-                        std::cout << "v\n";
-                        c = this->parse_line(line, p);
-                        break;
-                    case R:
-                        std::cout << "r\n";
-                        c = this->parse_line(line, p);
-                        break;
-                    case M:
-                        std::cout << "M, baby\n";
-                        c = this->parse_line(line, p);
-                        break;
-                    default:
-                        std::cout << "None?\n";
+        // Keeps track of type in regex list
+        unsigned int t = 0;
+
+        // Catch any syntax errors
+        try {
+            // Iterate through regex patterns; if match found, determine
+            // which matched based on regex order in const vector
+            for (auto reg: regexes) {
+                if (regex_search(line, matches, reg)) {
+                    switch (t) {
+                        case EMPTY_LINE:
+                            std::cout << "empty\n";
+                            break;
+                        case R:
+                        case L:
+                        case C:
+                        case D:
+                        case M:
+                            c = this->parse_line<Component>(matches, t);
+                            this->components.push_back(c);
+
+                            break;
+                        case VDC:
+                            std::cout << "v\n";
+
+                            s = this->parse_line<Source>(matches, t);
+                            this->sources.push_back(s);
+
+                            break;
+                        default:
+                            std::cout << "None?\n";
+                    }
+
+                    // Once found a match, no need to search more
+                    break;
                 }
 
-                this->components.push_back(c);
+                else
+                    throw parser_error();
 
-                // Once found a match, no need to search more
-                break;
+                t++;
             }
-
-            p++;
         }
 
+        catch (parser_error& e) {
+            std::cout << "Syntax error on line " << line_no << "." << std::endl;
+            break;
+        }
+
+        line_no++;
     }
 }
 
 void Parser::print_components() {
     for (auto c: this->components) {
-        std::cout << c->node1 << " " << c->node2 << std::endl;
+        std::cout << c->label << " " << c->n1 << " " << c->n2 << " " << c->n3 << std::endl;
     }
 }
 
-Component* Parser::parse_line(std::string line, unsigned char c) {
-    if (c == R)
-        return new Component(1, 1, 0, 30.45);
-    else if (c == VDC)
-        return new Component(5, 2, 3, 10.5);
-    else if (c == M)
-        return new Transistor(5, 2, 3, 1, 10.5);
+template<class T>
+T* Parser::parse_line(const std::smatch matches, unsigned char t) {
+    T* temp;
+
+    try {
+        if (t == VDC)
+            temp = new T(matches[1], t, matches[3], matches[4], matches[5]);
+        else if (t == M)
+            temp = new T(matches[1], t, matches[3], matches[4], matches[5]);
+        else
+            temp = new T(matches[1], t, matches[3], matches[4], matches[5]);
+    }
+
+    catch (std::out_of_range& e) {
+        std::cout << "Value out of range on line " << line_no << std::endl;
+        temp = nullptr;
+
+        throw parser_error();
+    }
+
+    return temp;
 }
--- src/parser.h
+++ src/parser.h
@@ -14,14 +14,23 @@
 
 class parser_error : public std::exception {
 public:
-    parser_error() { std::cout << "Dude!\n"; }
+    parser_error() {}
     ~parser_error() {}
 };
 
-enum component_type {
+template<typename T>
+void clear_vector(std::vector<T*>& v) {
+    for (auto e: v)
+        delete e;
+
+    v.clear();
+}
+
+enum type {
     R,
-    C,
     L,
+    C,
+    D,
     M,
     VDC,
     EMPTY_LINE,
@@ -30,42 +39,77 @@ enum component_type {
 
 class Component {
 public:
-    int label, node1, node2;
+    std::string label;
+
+    // R, L, C, etc.
+    int type;
+
+    // Nodes; 3rd for M
+    int n1, n2, n3;
+
     double value;
 
-    Component(int label, int node1, int node2, double value) {
+    // R, C, L, D, M
+    Component(std::string label, int type, int n1, int n2, double value) {
         this->label = label;
-        this->node1 = node1;
-        this->node2 = node2;
-        this->value = value;
+        this->n1 = n1;
+        this->n2 = n2;
+        this->type = type;
+
+        // Set sane defaults for value and n3
+        if (type != M) {
+            this->value = value;
+            this->n3 = -1;
+        }
+
+        else {
+            this->value = -1;
+            this->n3 = (int)value;
+        }
+    }
+
+    // Proxy constructor for direct calling
+    Component(std::string label, int type, std::string n1, std::string n2, std::string value) {
+        Component(label, type, std::stoi(n1, nullptr), std::stoi(n2, nullptr),
+                  std::stod(value, nullptr));
     }
 };
 
-class Transistor : public Component {
+class Source : public Component {
 public:
-    int node3;
+    Source(std::string label, int type, int n1, int n2, double value):
+    Component(label, type, n1, n2, value) {}
 
-    Transistor(int label, int node1, int node2, int node3, double value)
-    : Component(label, node1, node2, value) {
-        this->node3 = node3;
-    }
+    Source(std::string label, int type, std::string n1, std::string n2, std::string value):
+    Component(label, type, n1, n2, value) {}
 };
 
 class Parser {
-    // std::vector<Component*> two_terminal;
-    // std::vector<Transistor*> three_terminal;
-    // std::vector<Source*> sources;
     std::vector<Component*> components;
+    std::vector<Source*> sources;
+
     std::ifstream file;
+
+    unsigned int line_no = 1;
 
     // Regexes
     const std::vector<const std::regex> regexes = {
-        std::regex("^R", std::regex::icase),
-        std::regex("^C", std::regex::icase),
-        std::regex("^L", std::regex::icase),
-        std::regex("^M", std::regex::icase),
-        std::regex("^V", std::regex::icase),
-        std::regex(R"(^[ \r\t]*\n$)")
+        // Resistor, inductor, capacitor - split to simplify identification in loop
+        std::regex(R"(^(R(\d+)) (\d+) (\d+) (\d+\.?\d+?)$)", std::regex::icase),
+        std::regex(R"(^(L(\d+)) (\d+) (\d+) (\d+\.?\d+?)$)", std::regex::icase),
+        std::regex(R"(^(C(\d+)) (\d+) (\d+) (\d+\.?\d+?)$)", std::regex::icase),
+
+        // Diode
+        std::regex(R"(^(D(\d+)) (\d+) (\d+)$)", std::regex::icase),
+
+        // Transistor
+        std::regex(R"(^(M(\d+)) (\d+) (\d+) (\d+)$)", std::regex::icase),
+
+        // DC voltage source
+        std::regex(R"(^(V(\d+)) (\d+) (\d+) (\d+\.?\d+?)$)", std::regex::icase),
+
+        // Empty line; what is the problem?
+        std::regex(R"(^[\r\t]*\n$)")
     };
 
 public:
@@ -74,30 +118,30 @@ public:
         file.open(path);
 
         if (!file.is_open()) {
-            std::cout << "Invalid file.\n";
+            std::cout << "File does not exist.\n";
             exit(0);
         }
     }
 
     ~Parser() {
-        // std::cout << "Destroyed!" << std::endl;
-
-        // Free each and every component
-        for (auto c: components)
-            delete c;
-
-        // Shrink vector size to 0
-        components.clear();
+        std::cout << "Destroyed!" << std::endl;
 
         // Close file
         file.close();
+
+        // Clear up vectors
+        clear_vector<Component>(components);
+        clear_vector<Source>(sources);
     }
 
     void parse();
     void print_components();
 
 private:
-    Component* parse_line(std::string line, unsigned char c);
+    template<class T>
+    T* parse_line(const std::smatch, unsigned char);
+    // Component* parse_component(std::string line, std::smatch matches, unsigned char c);
+    // Source* parse_source(std::string line, std::smatch matches, unsigned char c);
 };
 
 #endif
